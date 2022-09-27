32ab9b93ddbb8835fa8a72c70c0691f40443ccf5
lavaspoon <lavaspoon@naver.com>
2022-09-27 12:47:58 +0900

    로그인뷰컨트롤러 네비게이션바 숨김처리

--- LoginModule/LoginViewController.swift
+++ LoginModule/LoginViewController.swift
@@ -8,5 +8,10 @@
 import UIKit
 
 class LoginViewController : UIViewController {
-    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //네비게이션바 숨김
+        navigationController?.navigationBar.isHidden = true
+    }
 }
