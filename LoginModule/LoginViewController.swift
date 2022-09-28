a8c7d14fbadffd549098efbcae5a4362fc13a8b0
lavaspoon <lavaspoon@naver.com>
2022-09-29 00:41:09 +0900

    버튼 스타일 적용 및 Outlet 연결

--- LoginModule/LoginViewController.swift
+++ LoginModule/LoginViewController.swift
@@ -8,10 +8,30 @@
 import UIKit
 
 class LoginViewController : UIViewController {
+    @IBOutlet weak var emailLoginBtn: UIButton!
+    @IBOutlet weak var googleLoginBtn: UIButton!
+    @IBOutlet weak var appleLoginBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //버튼 스타일 적용
+        [emailLoginBtn, googleLoginBtn, appleLoginBtn].forEach {
+            $0?.layer.borderWidth = 1
+            $0?.layer.borderColor = UIColor.white.cgColor
+            $0?.layer.cornerRadius = 30
+        }
+    }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         //네비게이션바 숨김
         navigationController?.navigationBar.isHidden = true
+    }
+    
+    @IBAction func googleLoginBtnTapped(_ sender: UIButton) {
+        
+    }
+    @IBAction func appleLoginBtnTapped(_ sender: UIButton) {
+        
     }
 }
