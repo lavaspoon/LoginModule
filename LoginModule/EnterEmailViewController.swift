8b5c07498918139e1495b3119f385e0f9207f10a
lavaspoon <lavaspoon@naver.com>
2022-10-02 00:20:46 +0900

    메인뷰컨트롤러 생성

--- LoginModule/EnterEmailViewController.swift
+++ LoginModule/EnterEmailViewController.swift
@@ -18,11 +18,36 @@ class EnterEmailViewController: UIViewController {
         
         nextBtn.layer.cornerRadius = 10
         nextBtn.isEnabled = false
+        
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        //화면을 켰을때 커서 위치 세팅
+        emailTextField.becomeFirstResponder()
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         //네비게이션 바 보이기
         navigationController?.navigationBar.isHidden = false
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+}
+
+extension EnterEmailViewController: UITextFieldDelegate {
+    //입력후 키보드 사라짐
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return false
+    }
+    //이메일, 비밀번호가 값일 있을때 버튼 활성화
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        let isEmailEmpty = emailTextField.text == ""
+        let isPasswordEmpty = passwordTextField.text == ""
+        nextBtn.isEnabled = !isEmailEmpty && !isPasswordEmpty
     }
 }
---
+++ LoginModule/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  LoginModule
+//
+//  Created by spoon lava on 2022/10/02.
+//
+
+import UIKit
+
+class MainViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+    }
+    
+}
