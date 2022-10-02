68addee973878dfbebdf1b1e9cb8a9fd5e5bf0c7
lavaspoon <lavaspoon@naver.com>
2022-10-03 00:20:42 +0900

    Firebase 초기화 코드 추가

--- LoginModule/AppDelegate.swift
+++ LoginModule/AppDelegate.swift
@@ -6,14 +6,17 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
+        [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        //Firebase 초기화
+        FirebaseApp.configure()
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 
